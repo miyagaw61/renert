8f3fb2b085455b451918b9e90f7b6e589d48fa96
miyagaw61 <miyagaw61@gmail.com>
2018-10-07 19:06:47 +0900

    Add result wrapping to SystemResult::new

--- src/lib.rs
+++ src/lib.rs
@@ -6,26 +6,24 @@ use colored::*;
 pub struct SystemResult {
     pub stdout: String,
     pub stderr: String,
-    pub status: i32
 }
 
 impl SystemResult {
-    fn new(output: std::process::Output) -> SystemResult {
+    fn new(output: std::process::Output) -> Result<SystemResult, SystemResult> {
         let mut stdout: Vec<char> = std::str::from_utf8(&output.stdout[..]).unwrap().to_string().chars().collect();
         stdout.pop();
         let stdout: String = stdout.into_iter().collect();
         let mut stderr: Vec<char> = std::str::from_utf8(&output.stderr[..]).unwrap().to_string().chars().collect();
         stderr.pop();
         let stderr: String = stderr.into_iter().collect();
-        let mut result = SystemResult {
+        let result = SystemResult {
             stdout: stdout,
             stderr: stderr,
-            status: 0
         };
         if result.stderr != "" {
-            result.status = 1
+            return Err(result);
         }
-        result
+        return Ok(result);
     }
 }
 
@@ -40,13 +38,13 @@ pub fn my_eprint(msg: String) {
     println!("{}", "=================================".red().to_string());
 }
 
-pub fn gen_systemresult(oput: std::process::Output) -> Result<SystemResult, SystemResult> {
-    let system_result = SystemResult::new(oput);
-    if system_result.status == 1 {
-        return Err(system_result);
-    }
-    return Ok(system_result);
-}
+//pub fn gen_systemresult(oput: std::process::Output) -> Result<SystemResult, SystemResult> {
+//    let system_result = SystemResult::new(oput);
+//    if system_result.status == 1 {
+//        return Err(system_result);
+//    }
+//    return Ok(system_result);
+//}
 
 pub fn system_on_shell(command: &str) -> Result<SystemResult, SystemResult> {
     let oput = Command::new("sh")
@@ -54,7 +52,7 @@ pub fn system_on_shell(command: &str) -> Result<SystemResult, SystemResult> {
         .arg(command)
         .output()
         .expect(format!("Failed to execute process: \"sh -c '{}'\"", command).as_str());
-    return gen_systemresult(oput);
+    return SystemResult::new(oput);
 }
 
 pub fn process_on_shell(command: &str) {
@@ -71,7 +69,7 @@ pub fn system(command: &[&str]) -> Result<SystemResult, SystemResult> {
         .args(&command[1..])
         .output()
         .expect(format!("Failed to execute process: \"sh -c '{}'\"", command.join(" ")).as_str());
-    return gen_systemresult(oput);
+    return SystemResult::new(oput);
 }
 
 pub fn process(command: &[&str]) {
